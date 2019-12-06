21fc43b64babab0c6429bb7578c85493b6f2449e
TERRUSS <olivier.benaben@protonmail.com>
2019-12-06 06:45:24 +0100

    ezr

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-
-const http = require('http');
-const fs = require('fs');
+const app = require('express')();
+const http = require("http");
+const https = require('https');
 const path = require('path');
+
+const fs = require('fs');
+
+let options = {
+	key: fs.readFileSync('/etc/letsencrypt/live/arveto.io/privkey.pem', 'utf8'),
+	cert: fs.readFileSync('/etc/letsencrypt/live/arveto.io/fullchain.pem', 'utf8'),
+}
+
+/******************************************************************************/
+
+app.use(require('express').static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/app', (req, res) => {
+    res.sendFile(__dirname + '/public/app.html');
+});
+
+https.createServer(options, app).listen(443);
+
+http.createServer(function (req, res) {
+	res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+	res.end();
+}).listen(80);
+
+
 const url = require('url');
 
-const { exec } = require('child_process');
-
 const mysql = require('mysql');
-const express = require('express');
-const app = express();
 
 const databaseWrapper = require('./db_wrapper.js');
 const config = {
@@ -33,6 +57,7 @@ app.use(bodyParser.json());
   serve();
 
 /*********************************************/
+
 
 
 function serve() {
